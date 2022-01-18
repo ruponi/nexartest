0e2a285fe7fb902f9790da67463e0b47a6238cdd
Ruslan Ponomarenko <ruponi@gmail.com>
2022-01-18 12:30:18 +0200

    Initial Commit

---
+++ NexarLocationTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NexarLocationTest
+//
+//  Created by Ruslan Ponomarenko on 1/18/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
